d5bc6f21d2a80145d1ac1016e4836fb06ba1b4d4
kosuke miyoshi <miyoshi@narr.jp>
2018-12-13 12:21:32 +0900

    pass model test

--- model.py
+++ model.py
@@ -8,61 +8,78 @@ import numpy as np
 def activation(x):
     return 1.0 / (1.0 + np.exp(-x))
 
-def noise():
-    return 0.0
+# TODO: サイズの指定
+def noise(size):
+    return np.random.normal(size=size)
 
 # 定数
-g_lk = 1.0
+g_lk = 0.1
+g_a = 0.8
 g_b = 1.0
-g_a = 1.0
 g_d = 1.0
-noise_delta = 1.0
+noise_delta = 0.1
 
 e_exc = 1.0   # Excitatory reversal potential
 e_inh = -1.0  # Inhibitory reversal potential
 
-g_som = 1.0
+g_som = 0.8
 
-# TODO: etaは層によって違う模様
-eta_pp_bu = 1.0
-eta_pp_td = 1.0
-eta_pi = 1.0
-eta_ip = 1.0
+# TODO: etaは層によって変える必要あり
+eta_pp_bu = 0.0011875
+eta_pp_td = 0.0011875
+eta_pi = 0.0002375
+eta_ip = 0.0005
+
+LAYER_TYPE_BOTTOM = 0
+LAYER_TYPE_HIDDEN = 1
+LAYER_TYPE_TOP    = 2
 
 
 class Layer(object):
-    def __init__(self, pd_unit_size, sst_unit_size):
+    def __init__(self, pd_unit_size, layer_type):
         self.pd_unit_size = pd_unit_size # 錐体細胞のユニット数
-        self.sst_unit_size = sst_unit_size # 錐体細胞のユニット数
+
+        self.layer_type = layer_type
         
         self.upper_layer = None
         self.lower_layer = None
-        
-        # Pyramidal neuron potentials
-        # 錐体細胞の電位
-        self.u_p = np.zeros([self.pd_unit_size]) # Soma
-        
-        # SST inter-neuraon potentials
-        # SSTインターニューロンの電位
-        self.u_i = np.zeros([self.sst_unit_size]) # Soma
+
+        if self.layer_type is not LAYER_TYPE_BOTTOM:
+            # Pyramidal neuron Soma potentials
+            self.u_p = np.zeros([self.pd_unit_size])
+        else:
+            # 最下層は入力としての発火率を入れる
+            self.exteral_r = np.zeros([self.pd_unit_size])
         
     def connect_to(self, upper_layer):
         self.upper_layer = upper_layer
         upper_layer.lower_layer = self
-        
+
+        if self.layer_type is not LAYER_TYPE_BOTTOM:
+            # 最下層レイヤーにはSST Interneuronが無い
+            self.sst_unit_size = upper_layer.pd_unit_size # 錐体細胞のユニット数
+            # SST inter-neuraon Soma potentials
+            self.u_i = np.zeros([self.sst_unit_size])
+
+            # PD -> SST
+            self.w_ip = np.random.uniform(-1, 1, size=(self.sst_unit_size,
+                                                       self.pd_unit_size))
+            # SST -> PD
+            self.w_pi = np.random.uniform(-1, 1, size=(self.pd_unit_size,
+                                                       self.sst_unit_size))            
+            
         # K -> K+1
         self.w_pp_bu = np.random.uniform(-1, 1, size=(upper_layer.pd_unit_size,
                                                       self.pd_unit_size))
         # K+1 -> K
         self.w_pp_td = np.random.uniform(-1, 1, size=(self.pd_unit_size,
                                                       upper_layer.pd_unit_size))
-        
-        # PD -> SST
-        self.w_ip = np.random.uniform(-1, 1, size=(self.sst_unit_size,
-                                                   self.pd_unit_size))
-        # SST -> PD
-        self.w_pi = np.random.uniform(-1, 1, size=(self.pd_unit_size,
-                                                   self.sst_unit_size))
+
+    def get_p_activation(self):
+        if self.layer_type == LAYER_TYPE_BOTTOM:
+            return self.exteral_r
+        else:
+            return activation(self.u_p)
         
     def calc_i_p(self):
         # トップLayerにおける教師信号によるcurrentを算出
@@ -84,59 +101,53 @@ class Layer(object):
         # Apical, Basalの電位をFiring Rateから計算
 
         # TODO: v_p_a, v_p_b, v_i_b はテンポラリ変数にできそうだが、
-        # update_weight()の中でも利用しているので、メンバ変数にしている. 要整理.        
+        # update_weight()の中でも利用しているので、メンバ変数にしている. 要整理.
         
         # Piramidal Apical
-        # (TopのLayerには無いので0に)
-        if self.upper_layer is not None:
+        if self.layer_type == LAYER_TYPE_HIDDEN:
             self.v_p_a = self.w_pp_td.dot(activation(self.upper_layer.u_p)) + \
                          self.w_pi.dot(activation(self.u_i))
             # (pd_unit_size)
-        else:
-            # TODO: 整理して最適化が必要
-            self.v_p_a = np.zeros([self.pd_unit_size])
         
         # Piramidal Basal
-        if self.lower_layer is not None:
-            self.v_p_b = self.lower_layer.w_pp_bu.dot(activation(self.lower_layer.u_p))
-        else:
-            # TODO: 整理して最適化が必要
-            self.v_p_b = np.zeros([self.pd_unit_size])
+        if self.layer_type == LAYER_TYPE_HIDDEN or self.layer_type == LAYER_TYPE_TOP:
+            self.v_p_b = self.lower_layer.w_pp_bu.dot(self.lower_layer.get_p_activation())
         
         # SST Basal
-        if self.upper_layer is not None:
-            # 最上位レイヤーでは、SSTが無い
+        if self.layer_type == LAYER_TYPE_HIDDEN:
             self.v_i_b = self.w_ip.dot(activation(self.u_p))
             # (sst_unit_size)
-        else:
-            # TODO: 整理して最適化が必要
-            self.v_i_b = np.zeros([self.sst_unit_size])
-        
-        # upperからSSTにながれるcurrent
-        if self.upper_layer is not None:
-            i_i = self.calc_i_i()
-        else:
-            i_i = 0.0
-        
-        # 教師信号によるcurrent
-        i_p = self.calc_i_p()
-        
-        # TODO: トップでは、g_aがゼロになる
-        
+
         # 錐体細胞の電位の更新式
-        d_u_p = -g_lk * self.u_p + \
-                g_b * (self.v_p_b - self.u_p) + \
-                g_a * (self.v_p_a - self.u_p) + \
-                i_p + \
-                noise_delta * noise()
-        self.u_p += d_u_p * dt
-        
+        if self.layer_type == LAYER_TYPE_TOP:
+            # 教師信号によるcurrent
+            # TODO:
+            #i_p = self.calc_i_p()
+            i_p = 0.0
+            
+            # 最終層にはApical無いがi_pがある
+            d_u_p = -g_lk * self.u_p + \
+                    g_b * (self.v_p_b - self.u_p) + \
+                    i_p + \
+                    noise_delta * noise(len(self.u_p))
+            self.u_p += d_u_p * dt
+        elif self.layer_type == LAYER_TYPE_HIDDEN:
+            # 中間層にはApicalがあるがi_pが無い
+            d_u_p = -g_lk * self.u_p + \
+                    g_b * (self.v_p_b - self.u_p) + \
+                    g_a * (self.v_p_a - self.u_p) + \
+                    noise_delta * noise(len(self.u_p))
+            self.u_p += d_u_p * dt
+
         # SSTインターニューロンの電位の更新式
-        d_u_i = -g_lk * self.u_i + \
-                g_d * (self.v_i_b - self.u_i) + \
-                i_i + \
-                noise_delta * noise()
-        self.u_i += d_u_i * dt
+        if self.layer_type == LAYER_TYPE_HIDDEN:
+            # upperからSSTにながれるcurrent
+            i_i = self.calc_i_i()
+            d_u_i = -g_lk * self.u_i + \
+                    g_d * (self.v_i_b - self.u_i) + \
+                    i_i + \
+                    noise_delta * noise(len(self.u_i))
+            self.u_i += d_u_i * dt
 
     def calc_d_weight(self, eta, post, pre):
         # 次元を増やす
@@ -146,32 +157,33 @@ class Layer(object):
         return d_w
     
     def update_weight(self, dt):
-        if self.upper_layer is None:
+        if self.layer_type == LAYER_TYPE_TOP:
             # 最上位レイヤーでは、Weight更新する部分が無い
             return
         
-        # この階層のPiramidalの発火率
-        r_p = activation(self.u_p)
+        r_p = self.get_p_activation()
         
-        # Bottom Up結線のweight更新
-        upper_r_p = activation(self.upper_layer.u_p)
-        upper_v_p_b_hat = self.upper_layer.v_p_b * (g_b / (g_lk + g_b + g_a))
-        d_w_pp_bu = self.calc_d_weight(eta_pp_bu, upper_r_p - upper_v_p_b_hat, r_p)
-        self.w_pp_bu += d_w_pp_bu * dt
+        if self.layer_type == LAYER_TYPE_HIDDEN or self.layer_type == LAYER_TYPE_BOTTOM:
+            # Bottom Up結線のweight更新
+            upper_r_p = activation(self.upper_layer.u_p)
+            upper_v_p_b_hat = self.upper_layer.v_p_b * (g_b / (g_lk + g_b + g_a))
+            d_w_pp_bu = self.calc_d_weight(eta_pp_bu, upper_r_p - upper_v_p_b_hat, r_p)
+            self.w_pp_bu += d_w_pp_bu * dt
         
-        # TopDownのPlasticyを使う場合
-        v_p_td_hat = self.w_pp_td.dot(upper_r_p)
-        d_w_pp_td = self.calc_d_weight(eta_pp_td, r_p - v_p_td_hat, upper_r_p)
-        self.w_pp_td += d_w_pp_td
+            # TopDownのPlasticyを使う場合
+            v_p_td_hat = self.w_pp_td.dot(upper_r_p)
+            d_w_pp_td = self.calc_d_weight(eta_pp_td, r_p - v_p_td_hat, upper_r_p)
+            self.w_pp_td += d_w_pp_td
         
         # P -> I結線のweight更新
-        r_i = activation(self.u_i)
-        v_i_b_hat = self.v_i_b * (g_d/g_lk + g_d)
-        d_w_ip = self.calc_d_weight(eta_ip, r_i - v_i_b_hat, r_p)
-        self.w_ip += d_w_ip * dt
+        if self.layer_type == LAYER_TYPE_HIDDEN:
+            r_i = activation(self.u_i)
+            v_i_b_hat = self.v_i_b * (g_d/g_lk + g_d)
+            d_w_ip = self.calc_d_weight(eta_ip, r_i - v_i_b_hat, r_p)
+            self.w_ip += d_w_ip * dt
         
-        # I -> P結線のweight更新
-        # (Apicalの電位を0に近づける)
-        v_rest = 0.0
-        d_w_pi = self.calc_d_weight(eta_pi, v_rest - self.v_p_a, r_i)
-        self.w_pi += d_w_pi * dt
+            # I -> P結線のweight更新
+            # (Apicalの電位を0に近づける)
+            v_rest = 0.0
+            d_w_pi = self.calc_d_weight(eta_pi, v_rest - self.v_p_a, r_i)
+            self.w_pi += d_w_pi * dt
--- model_test.py
+++ model_test.py
@@ -6,21 +6,21 @@ from __future__ import print_function
 import numpy as np
 import unittest
 
-from model import Layer
+from model import Layer, LAYER_TYPE_BOTTOM, LAYER_TYPE_HIDDEN, LAYER_TYPE_TOP
 
 
 class LayerTest(unittest.TestCase):
     def test_init(self):
         # sst_unit_sizeは上の階層のpd_unitサイズに等しくなる必要あり.
-        layer0 = Layer(pd_unit_size=64, sst_unit_size=32)
-        layer1 = Layer(pd_unit_size=32, sst_unit_size=10)
-        layer2 = Layer(pd_unit_size=10, sst_unit_size=5)
+        layer0 = Layer(pd_unit_size=30, layer_type=LAYER_TYPE_BOTTOM)
+        layer1 = Layer(pd_unit_size=20, layer_type=LAYER_TYPE_HIDDEN)
+        layer2 = Layer(pd_unit_size=10, layer_type=LAYER_TYPE_TOP)
         
         layer0.connect_to(layer1)
         layer1.connect_to(layer2)
         
-        dt = 0.01
-
+        dt = 0.1
+        
         layer0.update_potential(dt)
         layer1.update_potential(dt)
         layer2.update_potential(dt)
@@ -28,16 +28,16 @@ class LayerTest(unittest.TestCase):
         layer0.update_weight(dt)
         layer1.update_weight(dt)
         layer2.update_weight(dt)
-
+        
     def test_calc_d_weight(self):
-        layer = Layer(pd_unit_size=10, sst_unit_size=10)
+        layer = Layer(pd_unit_size=10, layer_type=LAYER_TYPE_HIDDEN)
         
         eta = 1.0
         post = np.zeros([10])
         pre = np.zeros([20])
         d_w = layer.calc_d_weight(eta, post, pre)
         self.assertEqual(d_w.shape, (10, 20))
-        
-        
+
+
 if __name__ == '__main__':
     unittest.main()
